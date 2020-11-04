7c356d0242092675e9b1f752f0f29748e3298d03
JosephB-Workstation <joeybloxsom@hotmail.com>
2020-11-04 15:58:47 -0500

    Implement verification command
    Implements the ability to change the necessary verification level for a discord server.

---
+++ cogs/verification.py
@@ -0,0 +1,27 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+class Verification(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    @commands.bot_has_permissions(manage_guild=True)
+    async def cverify(self, ctx, verificationlevel="none"):
+        guild = ctx.guild
+        if verificationlevel == "none" or verificationlevel == "low" or verificationlevel == "medium" or verificationlevel == "high" or verificationlevel == "table_flip" or verificationlevel == "extreme" or verificationlevel == "double_table_flip" or verificationlevel == "very_high":
+            await guild.edit(verification_level=discord.VerificationLevel[verificationlevel])
+            await ctx.send(f"Guild verification level set to {verificationlevel}!")
+        else:
+            await ctx.send("Invalid verification level! See \"w.help cverify\" for valid entries!")
+
+    @cverify.error
+    async def cverify_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send('Error: You do not have the required permission: *Manage Server* for this command!')
+        if isinstance(error, commands.BotMissingPermissions):
+            await ctx.send('Error: I not have the required permission: *Manage Server* for this command!')
+def setup(client):
+    client.add_cog(Verification(client))

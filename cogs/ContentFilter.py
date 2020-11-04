5c367945d45480668c6ab6696e74534739da0646
JosephB-Workstation <joeybloxsom@hotmail.com>
2020-11-04 16:06:16 -0500

    Implement Content filter command
    Implements the command to modify what content filter level is applied to the discord server.

---
+++ cogs/ContentFilter.py
@@ -0,0 +1,28 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+class ContentFilter(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    @commands.bot_has_permissions(manage_guild=True)
+    async def cfilter(self, ctx, filterLevel="disabled"):
+        guild = ctx.guild
+        if filterLevel == "disabled" or filterLevel == "no_role" or filterLevel == "all_members":
+            await guild.edit(explicit_content_filter =discord.ContentFilter[filterLevel])
+            await ctx.send(f"Guild content filter set to {filterLevel}!")
+        else:
+            await ctx.send("Invalid content filter! See \"w.help cfilter\" for valid entries!")
+
+    @cfilter.error
+    async def cfilter_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send('Error: You do not have the required permission: *Manage Server* for this command!')
+        if isinstance(error, commands.BotMissingPermissions):
+            await ctx.send('Error: I not have the required permission: *Manage Server* for this command!')
+
+def setup(client):
+    client.add_cog(ContentFilter(client))

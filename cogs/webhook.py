0caa3071282ccafbea7aff21ac323734df3ff047
JosephB-Workstation <joeybloxsom@hotmail.com>
2020-11-05 16:42:30 -0500

    Implements delwebhook
    Allows users to delete a single specified webhook

--- cogs/webhook.py
+++ cogs/webhook.py
@@ -41,6 +41,44 @@ class Webhooks(commands.Cog):
             await ctx.send("Sorry, this command only works for text channels!")
 
 
+    @commands.command()
+    async def delwebhook(self, ctx, webhookid: int=1, targetid: int=1):
+        channel = None
+        if targetid == 1:
+            channel = ctx.channel
+        else:
+            await self.client.wait_until_ready()
+            channel = self.client.get_channel(targetid)
+
+        if (channel == None):
+            await ctx.send("Channel not found.")
+            return
+
+        permission = channel.permissions_for(ctx.author)
+        permission2 = channel.permissions_for(ctx.guild.me)
+
+        if permission.manage_channels != True:
+            await ctx.send('Error: You do not have the required permission: *Manage channels* for this command!')
+        elif permission2.manage_channels != True:
+            await ctx.send('Error: I not have the required permission: *Manage channels* for this command!')
+        elif permission.manage_webhooks != True:
+            await ctx.send('Error: You do not have the required permission: *Manage webhooks* for this command!')
+        elif permission2.manage_webhooks != True:
+            await ctx.send('Error: I not have the required permission: *Manage webhooks* for this command!')
+        elif isinstance(channel, discord.TextChannel):
+            hooks = await channel.webhooks()
+            if len(hooks) == 0:
+                await ctx.send("No webhooks in this channel!")
+            else:
+                for webhook in hooks:
+                    if webhook.id == webhookid:
+                        await ctx.send("Webhook found and deleted!")
+                        await webhook.delete();
+                        return;
+                await ctx.send("Webhook not found! Run w.webhooks to get a list of webhooks and their IDs!")
+        else:
+            await ctx.send("Sorry, this command only works for text channels!")
+
 
 def setup(client):
     client.add_cog(Webhooks(client))

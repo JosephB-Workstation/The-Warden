7bb8c2a000a2a1b5fee15754f7b0daef7ef6606b
JosephB-Workstation <joeybloxsom@hotmail.com>
2020-11-12 22:10:18 -0500

    Adds cafkch and cafktime commands
    to edit the server's afk voice channel and timing for the voice channel

--- cogs/GUILD.py
+++ cogs/GUILD.py
@@ -32,6 +32,50 @@ class GuildEdit(commands.Cog):
         else:
             await ctx.send("Region not found! run w.help cregion for a list of known regions!")
 
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    @commands.bot_has_permissions(manage_guild=True)
+    async def cafkch(self, ctx, targetid: int=1):
+        guild = ctx.guild
+        channelID = str(targetid)
+        await self.client.wait_until_ready()
+        channel = self.client.get_channel(targetid)
+        if isinstance(channel, discord.VoiceChannel):
+            await guild.edit(afk_channel=channel)
+            await ctx.send(f"Server inactive channel changed to **{channel.name}**!")
+        else:
+            await ctx.send("Sorry, but this command requires the ID of a voice channel!")
+
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    @commands.bot_has_permissions(manage_guild=True)
+    async def cafktime(self, ctx, seconds):
+        guild = ctx.guild
+        time = None
+        code = None
+        if seconds == "1min":
+            time = int(60)
+            code = str("1 Minute")
+        elif seconds == "5min":
+            time = int(300)
+            code = str("5 Minutes")
+        elif seconds == "15min":
+            time = int(900)
+            code = str("15 Minutes")
+        elif seconds == "30min":
+            time = int(1800)
+            code = str("30 Minutes")
+        elif seconds == "1hour":
+            time = int(3600)
+            code = str("1 Hour")
+
+        if time != None:
+            await guild.edit(afk_timeout=time)
+            await ctx.send(f"Afk channel timeout set to **{code}**")
+        else:
+            await ctx.send("Invalid time! Please see w.help cafktime for valid time entries!")
+
+
 
     @csname.error
     async def csname_error(self, ctx, error):
@@ -44,6 +88,24 @@ class GuildEdit(commands.Cog):
 
     @cregion.error
     async def cregion_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send('Error: You do not have the required permission: *Manage Server* for this command!')
+        if isinstance(error, commands.BotMissingPermissions):
+            await ctx.send('Error: I not have the required permission: *Manage Server* for this command!')
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('Error: You have reached the rate limit for this command. Apologies for the inconvienence. Please try again later.')
+
+    @cafkch.error
+    async def cafkch_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send('Error: You do not have the required permission: *Manage Server* for this command!')
+        if isinstance(error, commands.BotMissingPermissions):
+            await ctx.send('Error: I not have the required permission: *Manage Server* for this command!')
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('Error: You have reached the rate limit for this command. Apologies for the inconvienence. Please try again later.')
+
+    @cafktime.error
+    async def cafktime_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send('Error: You do not have the required permission: *Manage Server* for this command!')
         if isinstance(error, commands.BotMissingPermissions):
